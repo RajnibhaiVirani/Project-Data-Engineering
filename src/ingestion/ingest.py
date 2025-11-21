a128328911895709ec8b343d8deaf7edba369897
RajnibhaiVirani <viranirajnibhai@gmail.com>
2025-11-21 22:54:39 +0100

    Final fixes and cleanup

--- src/ingestion/ingest.py
+++ src/ingestion/ingest.py
@@ -1,17 +1,18 @@
 import os
 import sys
 from minio import Minio
+from minio.error import S3Error
 from tenacity import retry, stop_after_delay, wait_fixed, retry_if_exception_type
 
 # My Configuration Variables
 MINIO_HOST = os.getenv("MINIO_ENDPOINT", "minio_storage:9000")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "admin_user")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "secure_password")
-BUCKET_NAME = "raw-data-zone"
-SOURCE_FILE = "/app/data/bankdataset.csv"
-DEST_FILE = "bankdataset.csv"
+BUCKET = "raw-data-zone"
+LOCAL_FILE = "/app/data/bankdataset.csv"
+OBJECT_NAME = "bankdataset.csv"
 
-def get_minio_client():
+def get_client():
     # DEBUG: Printing credentials to verify they match docker-compose
     print(f"DEBUG: Connecting to {MINIO_HOST}")
     print(f"DEBUG: User={MINIO_ACCESS_KEY}")
@@ -19,21 +20,24 @@ def get_minio_client():
     return Minio(MINIO_HOST, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
 
 @retry(retry=retry_if_exception_type(Exception), wait=wait_fixed(5), stop=stop_after_delay(60))
-def upload_to_lake(client):
+def upload_file():
+    client = get_client()
+
     # I added a check here: If the bucket doesn't exist, create it.
-    if not client.bucket_exists(BUCKET_NAME):
-        client.make_bucket(BUCKET_NAME)
-        print(f"--> Created new bucket: {BUCKET_NAME}")
-    
-    print(f"--> Uploading {DEST_FILE} to the Data Lake...")
-    client.fput_object(BUCKET_NAME, DEST_FILE, SOURCE_FILE)
+    if not client.bucket_exists(BUCKET):
+        client.make_bucket(BUCKET)
+        print(f"--> Created new bucket: {BUCKET}")
+    else:
+        print(f"--> Bucket already exists: {BUCKET}")
+
+    print(f"--> Uploading {OBJECT_NAME} to the Data Lake...")
+    client.fput_object(BUCKET, OBJECT_NAME, LOCAL_FILE)
     print("--> Success! File is safely stored in MinIO.")
 
 if __name__ == "__main__":
     print("--- Starting Ingestion Service ---")
     try:
-        client = get_minio_client()
-        upload_to_lake(client)
+        upload_file()
     except Exception as e:
         print(f"!!! Ingestion Failed: {e}")
         sys.exit(1)
